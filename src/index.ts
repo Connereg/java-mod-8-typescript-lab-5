f6cdd556032ccf135a38f6a03eef045f9da635df
Conner Gleason <connereg@optonline.net>
2022-08-16 20:46:19 -0400

    Lab 3 Completed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 // your code here
-const toPrependInput = (document.getElementById("spacesNumber") as HTMLInputElement).value;
-const toPrependToInput = (document.getElementById("stringToPrepend") as HTMLInputElement).value;
+const toPrependInput = (document.getElementById("spacesNumber") as HTMLInputElement);
+const toPrependToInput = (document.getElementById("stringToPrepend") as HTMLInputElement);
 const prependButton = document.getElementById("prependButton") as HTMLButtonElement;
 
-const numberInputArray = toPrependInput.split(", ");
-const stringInputArray = toPrependToInput.split(", ");
+const numberInputArray = toPrependInput!.value.split(", ");
+const stringInputArray = toPrependToInput!.value.split(", ");
 
 // your code here
 prependButton!.addEventListener("click", function () {
     console.log("Trigger working...")
+    console.log(numberInputArray)
+    console.log(stringInputArray)
     let prependResult = prepend(numberInputArray, stringInputArray);
     console.log(prependResult)
 
@@ -18,7 +20,8 @@ function prepend<T>(spaces: T[], strings: T[]) {
     let newArray = [...strings]
     
     for (let i = spaces.length - 1; i >= 0; i--) {
-        insertAtBeginning(spaces[i], strings, newArray)
+        console.log(insertAtBeginning(spaces[i], strings, newArray))
+        
     }
 
     return newArray  
