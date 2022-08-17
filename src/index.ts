2d083409ef231f3c10d21a8302c3a624bceba306
Conner Gleason <connereg@optonline.net>
2022-08-17 10:39:14 -0400

    Further progress, still needs work

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-// your code here
-const toPrependInput = (document.getElementById("spacesNumber") as HTMLInputElement);
-const toPrependToInput = (document.getElementById("stringToPrepend") as HTMLInputElement);
+
 const prependButton = document.getElementById("prependButton") as HTMLButtonElement;
 
-const numberInputArray = toPrependInput!.value.split(", ");
-const stringInputArray = toPrependToInput!.value.split(", ");
+
+// your code here
+
 
 // your code here
 prependButton!.addEventListener("click", function () {
+    const toPrependInput = (document.getElementById("spacesNumber") as HTMLInputElement);
+    const toPrependToInput = (document.getElementById("stringToPrepend") as HTMLInputElement);
+    const numberInputArray = toPrependInput!.value.split(", ");
+    const stringInputArray = toPrependToInput!.value.split(", ");
+
     console.log("Trigger working...")
-    console.log(numberInputArray)
-    console.log(stringInputArray)
+    console.log("Starting Spaces Numbers: " + numberInputArray)
+    console.log("Starting Strings: " + stringInputArray)
     let prependResult = prepend(numberInputArray, stringInputArray);
     console.log(prependResult)
 
 });
 
 function prepend<T>(spaces: T[], strings: T[]) {
-    let newArray = [...strings]
-    
-    for (let i = spaces.length - 1; i >= 0; i--) {
-        console.log(insertAtBeginning(spaces[i], strings, newArray))
-        
+
+    let spacesString: String = ""
+    for (let i=0; i < spaces[0]; i++) {
+        spacesString = spacesString + " "
     }
+    let resultString: T = spacesString + strings[0];
 
-    return newArray  
+    return resultString
+    
 }
 
-function insertAtBeginning<T>(spaces: T, strings: T[], newArray: T[]) {
-    newArray = [spaces, ... strings]
-    return newArray
-}
+// function insertAtBeginning(spacesString: String, stringToPrepend: String) {
+//     let resultString: String = spacesString + stringToPrepend
+//     return resultString
+// }
